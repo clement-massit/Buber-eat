8c1d99ee753e75379bdcb62a5a6340a416ca42f3
clement-massit <clement.massit@gmail.com>
2021-12-01 11:30:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import './App.css';

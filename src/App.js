a945d607c1182a6f36a1d68179b702b9adc5b37b
clement-massit <clement.massit@gmail.com>
2022-01-19 18:11:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,29 +5,39 @@ import Footer from "./components/footer";
 import './App.css';
 
 
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Pricing from "./pages/Pricing";
 import Team from "./pages/Team";
 import Contact from "./pages/Contact";
 
+
 function App() {
   return (
 
     <Router>
-      
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/products" element={<Products />} />
-          <Route exact path="/pricing" element={<Pricing />} />
-          <Route exact path="/team" element={<Team />} />
-          <Route exact path="/contact" element={<Contact />} />
 
-        </Routes>
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/pricing" element={<Pricing />} />
+        <Route exact path="/team" element={<Team />} />
+        <Route exact path="/contact" element={<Contact />} />
+
+      </Routes>
+      
+      <Footer>
+      
+      </Footer >
+      
+      
+     
+      
 
       
-      <Footer />
     </Router>
 
 
